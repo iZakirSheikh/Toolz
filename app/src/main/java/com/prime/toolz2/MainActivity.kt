c2dc0be5f7c4d4bcfc9661b96e48412796c9b212
Zakir Ahmad Sheikh <zikr673@gmail.com>
2022-05-19 22:18:06 +0530

    Initial commit

---
+++ app/src/main/java/com/prime/toolz2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.prime.toolz2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
