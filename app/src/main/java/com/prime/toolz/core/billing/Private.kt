1206e8407646db4c766b9d054a63fa2a5f9c060c
ZakirSheikh <sheikhzakirahmad@gmail.com>
2023-08-21 18:56:16 +0530

    work_flow_demo7

---
+++ app/src/main/java/com/prime/toolz/core/billing/Private.kt
@@ -0,0 +1,24 @@
+package com.prime.toolz.core.billing
+
+object Private {
+    /**
+     * Base64-encoded RSA public key to include in your app binary
+     */
+    const val PLAY_CONSOLE_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr5hogvST8wAaJJ7LmFV4UtUuEfmG0yjSP7kY/VKamOlk/Sl85sMmssvDL2tPOFH8N7vZ+0LEJsTD7SFzSbGMzmYaFejvmb+SJZTiGg6aPTpD6Iy5Q2diX1sWaQwhIDEEy+amCvqN/GTCSodS8sGOKL32i5/UZ2BXAIP1QLt9Zv9n76o8RUCigWMhRIbKXPHD5/kd5xbtz6B2WcGaG75VGkP2thBvSzP6A24lgCcDex3XdlIG06DbpruN78JlruCq10auYEFj9B6kLJx/z5Gpui3feGO5epjPRggz0PlT/2c9OS2hSZmsvMdPr8DYz4pdl+GalOIsOInyMHFSrlvcRwIDAQAB"
+    const val UNITY_APP_ID = "4893975"
+}
+
+object Product {
+
+    const val DISABLE_ADS = "remove_ads"
+
+}
+
+object Placement {
+
+    const val INTERSTITIAL = "interstitial"
+
+    const val BANNER_SETTINGS = "banner_settings"
+
+    const val BANNER_UNIT_CONVERTER = "banner_unit_converter"
+}
